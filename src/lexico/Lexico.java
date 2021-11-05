6288d4ec09e579d1968c1e55bb38e59ac97f2179
axl72 <axell.bernabel@unmsm.edu.pe>
2021-11-05 05:24:20 -0500

    Scanner beta

--- src/lexico/Lexico.java
+++ src/lexico/Lexico.java
@@ -9,13 +9,70 @@ package lexico;
  *
  * @author axell
  */
-public class Lexico {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Scanner {
+    private String entrada;
+    private int j;
+    
+    public Scanner(){
+        entrada = "";
+        j = 0;
     }
     
+    public void setEntrada(String entrada) {
+        this.entrada = entrada;
+    }
+    
+    public String obtenerToken(){
+        
+        StringBuffer token = new StringBuffer();
+        char actual;
+        
+        while(entrada.charAt(j) == ' '){
+            j++;
+        }
+        
+        if(j > entrada.length()){
+            token.append("$");
+        }
+        actual = entrada.charAt(j);
+        //para identificadores
+        if(actual >= 'a' && actual <= 'z' || (actual >= 'A' && actual <= 'Z')){
+            while((actual >= 'a' && actual <= 'z') || (actual >= 'A' && actual <= 'Z') || actual >= '0' && actual <= '9'){
+                token.append(actual);
+                j++;
+                actual = entrada.charAt(j);
+            }
+        }
+        
+        //para numeros
+        else if(actual >= '0' && actual <= '9'){
+            while(actual >= '0' && actual <= '9'){
+                token.append(actual);
+                j++;
+                actual = entrada.charAt(j);
+            }
+            //Para todo simbolo extraño
+        }else{
+            //System.out.println(actual);
+            token.append(actual);
+            j++;
+            actual = entrada.charAt(j);
+        }
+        
+        return token.toString();
+        
+    }
+    
+    public static void main(String[]args){
+        Scanner sc = new Scanner();
+        sc.setEntrada("xxxx = suma * a21 + 35;");
+        System.out.println(sc.obtenerToken());
+        System.out.println(sc.obtenerToken());
+        System.out.println(sc.obtenerToken());
+        System.out.println(sc.obtenerToken());
+        System.out.println(sc.obtenerToken());
+        System.out.println(sc.obtenerToken());
+        System.out.println(sc.obtenerToken());
+    }
 }
+
